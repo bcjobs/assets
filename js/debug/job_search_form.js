d2656d5f6a4383dd5ddf5a4ef5e58183d3d4212b
Johnny Oshika <johnnyoshika@gmail.com>
2015-07-31 12:02:04 -0700

    Added job_search_form.js

---
+++ js/debug/job_search_form.js
@@ -0,0 +1,50 @@
+﻿var JOBCENTRE = window.JOBCENTRE || {};
+
+JOBCENTRE.jobSearchForm = (function ($) {
+
+    return {
+        init: function (options) {
+            $('input[name=location]').select2({
+                minimumInputLength: 3,
+                allowClear: true,
+                ajax: {
+                    url: options.restPath + 'locations?pageSize=5&types=town%2Cprovince%2Cterritory',
+                    dataType: 'json',
+                    data: function (term, page) {
+                        return {
+                            q: term
+                        };
+                    },
+                    results: function (response, page) {
+                        return { results: response.data };
+                    }
+                },
+                initSelection: function ($element, callback) {
+                    $.ajax({
+                        url: options.restPath + 'locations?pageSize=1&types=town%2Cprovince%2Cterritory',
+                        data: { q: $element.val() },
+                        dataType: 'json',
+                        type: 'GET',
+                        success: function (response, textStatus, jqXHR) {
+                            if (response.data.length)
+                                callback(response.data[0]);
+                        }
+                    });
+                },
+                id: function (location) {
+                    if (location)
+                        return location.description;
+                    else
+                        return null;
+                },
+                formatResult: function (location) {
+                    return location.description;
+                },
+                formatSelection: function (location) {
+                    return location.description;
+                }
+            });
+        }
+    }
+
+}(jQuery));
