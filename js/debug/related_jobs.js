1f08a6f0b0ea89d339dee01c6318418b173b56e7
Logan Hollins <Logan.hollins1@gmail.com>
2017-03-13 15:30:06 -0700

    Move functions for controlling related_job visibility outside of global scope

--- js/debug/related_jobs.js
+++ js/debug/related_jobs.js
@@ -1,26 +1,22 @@
-var JOBCENTRE = window.JOBCENTRE || {};
+(function ($) {
+    var relatedJobsNextItem = 3;
+    $('[data-related-wrapper]').on('click', '[data-related-close]', function(e){
+        e.preventDefault();
+        $(this)
+            .closest('[data-related-link]')
+            .slideUp();
 
-JOBCENTRE.relatedJobs = (function ($) {
-  return {
-    hideRelatedJob: function(ele, e){
-      e.preventDefault();
-      $('[data-related-id=' + parseInt($(ele).attr("data-related-id")) + ']').hide();
-      if(JOBCENTRE.relatedJobsNextItem === undefined){
-        //Initialize to 4 as we start out displaying 3 jobs
-        JOBCENTRE.relatedJobsNextItem = 4;
-      }
-      var newRelatedJob = $('[data-related-id=' + JOBCENTRE.relatedJobsNextItem++ + ']');
-      if(newRelatedJob !== undefined){
-          // Remove style attribute to reveal element without breaking css
-          newRelatedJob.removeAttr("style");
-      }
-    },
-    optimizeRelatedJobs: function(){
-      if(window.optimize !== undefined && window.optimize.relatedJobs === 1){
-        $(".related-jobs-optimize").show();
-        $(".related-jobs-original").hide();
-      }
+        $('[data-related-wrapper] [data-related-link]')
+            .eq(relatedJobsNextItem++)
+            .slideDown();
+    });
+
+    if(window.optimize !== undefined && window.optimize.relatedJobs === 1){
+        $('[data-related-type="optimized"]')
+            .show()
+            .find('[data-related-link]:gt('+ (relatedJobsNextItem - 1) + ')')
+            .hide();
+    } else {
+        $('[data-related-type="default"]').show();
     }
-  };
-
 }(jQuery));
