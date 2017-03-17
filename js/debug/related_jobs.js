b3df8b022851b95e3c44aa927812c2b562ec9ad0
Logan Hollins <Logan.hollins1@gmail.com>
2017-03-17 15:19:26 -0700

    Add push function that is called from Google Optimize

--- js/debug/related_jobs.js
+++ js/debug/related_jobs.js
@@ -10,13 +10,19 @@
             .eq(relatedJobsNextItem++)
             .slideDown();
     });
-
-    if(window.optimize !== undefined && window.optimize.relatedJobs === 1){
+    var push = function(enable){
+      if(enable)
         $('[data-related-type="optimized"]')
             .show()
             .find('[data-related-link]:gt('+ (relatedJobsNextItem - 1) + ')')
             .hide();
-    } else {
+      else
         $('[data-related-type="default"]').show();
+
+      appInsights.trackEvent('RecommendedJobs', {'mainJob': JOBCENTRE.ai.mainJob, 'recommendedJobs': JOBCENTRE.ai.recommendedJobs, 'optimized': enable});
     }
+    if(JOBCENTRE.optimize.length === 0)
+      JOBCENTRE.optimize = {push: push};
+    else
+      push(JOBCENTRE.optimize[0]);
 }(jQuery));
