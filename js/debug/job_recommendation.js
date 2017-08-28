edf4efad9a33176cfa33cf654338d8cf2d2e4bf5
Logan Hollins <Logan.hollins1@gmail.com>
2017-08-28 12:29:56 -0700

    Change sessionStorage -> localStorage; Allow jobId to be int or string

--- js/debug/job_recommendation.js
+++ js/debug/job_recommendation.js
@@ -1,16 +1,16 @@
 (function ($) {
     var click = function(jobid, recommender){
-      if(!JOBCENTRE.capabilities.sessionStorage)
+      if(!JOBCENTRE.capabilities.localStorage)
           return;
 
-      var click = {'jobId': jobid, 'recommender': recommender, 'timestamp': new Date().toISOString()};
+      var click = {'jobId': parseInt(jobid), 'recommender': recommender, 'timestamp': new Date().toISOString()};
 
-      var clickArray = JSON.parse(window.sessionStorage.getItem('recommendationInfo'));
+      var clickArray = JSON.parse(window.localStorage.getItem('recommendationInfo'));
       if(clickArray == null)
           clickArray = [];
 
       clickArray.push(click);
-      window.sessionStorage.setItem('recommendationInfo', JSON.stringify(clickArray));
+      window.localStorage.setItem('recommendationInfo', JSON.stringify(clickArray));
     };
 
     var view = function(jobId){
@@ -22,22 +22,28 @@
     };
 
     var addEvent = function(jobId, jobEvent){
-      if(!JOBCENTRE.capabilities.sessionStorage)
+      if(!JOBCENTRE.capabilities.localStorage)
           return;
 
-      var clickArray = JSON.parse(window.sessionStorage.getItem('recommendationInfo'));
+      var clickArray = JSON.parse(window.localStorage.getItem('recommendationInfo'));
       if(clickArray == null)
           return;
 
       // Only find index if jobEvent hasn't happened previously
-      var clickIndex = clickArray.filter(function(e){return e[jobEvent] === undefined;}).map(function(e){return e.jobId;}).indexOf(jobId);
+      var clickIndex = clickArray.map(function(e){return e.jobId;}).indexOf(parseInt(jobId));
 
-      if(clickIndex == -1)
+      // return if the jobEvent has already happened or if more than 24h have passed
+      if(clickIndex == -1 || clickArray[clickIndex][jobEvent] !== undefined || hoursBetweenDates(new Date(clickArray[clickIndex].timestamp), new Date()) > 24)
           return;
 
       clickArray[clickIndex][jobEvent] = true;
-      window.sessionStorage.setItem('recommendationInfo', JSON.stringify(clickArray));
+      clickArray[clickIndex].timestamp = new Date().toISOString();
+      window.localStorage.setItem('recommendationInfo', JSON.stringify(clickArray));
       appInsights.trackEvent('JobseekerRecommendedJob' + jobEvent, {'jobId': clickArray[clickIndex].jobId, 'recommender': clickArray[clickIndex].recommender});
+    };
+
+    var hoursBetweenDates = function(d1, d2){
+      return Math.abs(d1.getTime() - d2.getTime()) / (1000 * 60 * 60);
     };
 
     JOBCENTRE.jobRecommendation = {click:click, view:view, apply:apply};
