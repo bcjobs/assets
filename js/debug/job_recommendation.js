b00434c4bbd64cdf0ef8e609fb27cf89034fc8cb
Logan Hollins <Logan.hollins1@gmail.com>
2017-08-29 10:07:14 -0700

    Move all code into try catch block; make jobId a string

--- js/debug/job_recommendation.js
+++ js/debug/job_recommendation.js
@@ -5,16 +5,16 @@
 
         var clicks = {};
         try {
+            jobId += '';
             clicks = JSON.parse(localStorage.getItem('recommendationInfo'));
+            if(clicks == null)
+                clicks = {};
+
+            clicks[jobId] = {'recommender': recommender, 'timestamp': new Date().getTime()};
+            localStorage.setItem('recommendationInfo', JSON.stringify(clicks));
         } catch (e) {
             // Continue if it fails
         }
-
-        if(clicks == null)
-            clicks = {};
-
-        clicks[jobId] = {'recommender': recommender, 'timestamp': new Date().getTime()};
-        localStorage.setItem('recommendationInfo', JSON.stringify(clicks));
     };
 
     var view = function(jobId){
@@ -31,22 +31,23 @@
 
         var clicks;
         try {
+            jobId += '';
             clicks = JSON.parse(localStorage.getItem('recommendationInfo'));
+
+            var click = clicks[jobId];
+            // return if the jobEvent has already happened or if more than 24h have passed
+            if (!click || click[jobEvent] || (new Date().getTime() - click.timestamp) / (1000 * 60 * 60) > 24)
+                return;
+
+            click[jobEvent] = true;
+            click.timestamp = new Date().getTime();
+
+            clicks[jobId] = click;
+            localStorage.setItem('recommendationInfo', JSON.stringify(clicks));
+            appInsights.trackEvent('JobseekerRecommendedJob' + jobEvent, {'jobId': jobId, 'recommender': click.recommender});
         } catch (e) {
             return;
         }
-
-        var click = clicks[jobId];
-        // return if the jobEvent has already happened or if more than 24h have passed
-        if (!click || !click.timestamp || !click.recommender || click[jobEvent] || (new Date().getTime() - click.timestamp) / (1000 * 60 * 60) > 24)
-            return;
-
-        click[jobEvent] = true;
-        click.timestamp = new Date().getTime();
-
-        clicks[jobId] = click;
-        localStorage.setItem('recommendationInfo', JSON.stringify(clicks));
-        appInsights.trackEvent('JobseekerRecommendedJob' + jobEvent, {'jobId': jobId, 'recommender': click.recommender});
     };
 
     JOBCENTRE.jobRecommendation = {click:click, view:view, apply:apply};
